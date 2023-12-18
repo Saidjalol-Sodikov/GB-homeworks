66e39582102e7a334d6f08a59385d3afafd43ec7
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-19 04:35:08 +0500

    Homework of Lesson06 is ready for checking

---
+++ Lesson06/Task03/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Задайте произвольную строку.
+// Выясните, является ли она палиндромом.
+
+string InputString(string msg)
+{
+    Console.Write(msg);
+    string result = Console.ReadLine();
+    return result;
+}
+bool IsStrPallindrom(string msg)
+{
+    bool result = true;
+    for (int i = 0; i < (msg.Length)/2; i++)
+    {
+        if (msg[i] != msg[msg.Length - 1-i])
+        {
+            result = false;
+            return result;
+        }
+    }
+    return result;
+}
+
+string strMain = InputString("Введите строку: ");
+if (IsStrPallindrom(strMain))
+{
+    Console.WriteLine("Да, строка является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Нет, строка не является палиндромом.");
+}
