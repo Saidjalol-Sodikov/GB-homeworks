fd3a38dfb985710f8a684568a092cbf765419b61
Saidjalol Sodikov <ssodikovu@gmail.com>
2024-02-25 01:34:26 +0500

    2,3 - пункт меню: работает

--- phonebook/phonebook.py
+++ phonebook/phonebook.py
@@ -4,24 +4,27 @@ def work_with_phonebook():
 
     phone_book=read_txt('phonebook.txt')
 
-    while (choice!=7):
+    while (choice!=10):
 
         if choice==1:
             print_result(phone_book)
         elif choice==2:
-            last_name=input('lastname ')
-            print(find_by_lastname(phone_book,last_name))
+            last_name=input('Фамилия: ')
+            print(*find_by_lastname(phone_book,last_name))
         elif choice==3:
+            corrent_number=input('Номер: ')
+            print(*find_by_number(phone_book,corrent_number))
+        elif choice==4:
             last_name=input('lastname ')
             new_number=input('new  number ')
             print(change_number(phone_book,last_name,new_number))
-        elif choice==4:
+        elif choice==5:
             lastname=input('lastname ')
             print(delete_by_lastname(phone_book,lastname))
-        elif choice==5:
+        elif choice==6:
             number=input('number ')
             print(find_by_number(phone_book,number))
-        elif choice==6:
+        elif choice==7:
             user_data=input('new data ')
             add_user(phone_book,user_data)
             write_txt('phonebook.txt',phone_book)
@@ -50,7 +53,7 @@ def read_txt(filename):
             phone_book.append(record)	
     return phone_book
 
-def write_txt(filename , phone_book):
+def write_txt(filename, phone_book):
 
     with open('phonebook.txt','w',encoding='utf-8') as phout:
 
@@ -63,10 +66,24 @@ def write_txt(filename , phone_book):
 
             phout.write(f'{s[:-1]}\n')
 
-def print_result(dictionary):
-    print(*[j for j in dictionary[0].keys()])
-    for i in dictionary:
-        print(*[j for j in i.values()], end='')
+def print_result(phone_book):
+    print(*[head for head in phone_book[0].keys()])
+    for contact in phone_book:
+        print(*[string for string in contact.values()], end='')
     print()
+
+def find_by_lastname(phone_book,last_name):
+    result = list()
+    for contact in phone_book:
+        if contact['Фамилия']==last_name:
+            result = [string for string in contact.values()]
+    return result
+
+def find_by_number(phone_book,number):
+    result = list()
+    for contact in phone_book:
+        if contact['Телефон']==number:
+            result = [string for string in contact.values()]
+    return result
 
 work_with_phonebook()
